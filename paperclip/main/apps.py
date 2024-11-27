0905eb0343cb0af9160634d4e0ccdf16ee03e310
dog0690 <charleymaivn@gmail.com>
2024-11-27 12:06:20 -0500

    paperclip project

--- paperclip/main/apps.py
+++ paperclip/main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class KnicksandnacksConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'knicksandnacks'
+    name = 'main'
